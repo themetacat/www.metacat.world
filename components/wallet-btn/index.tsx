ef2a3ec7591f82317c4a168e352a258bcfb7cb43
张宇博 <9840072+ZhangYuBoYa@user.noreply.gitee.com>
2022-05-07 10:31:46 +0800

    fix: wallet connect

--- components/wallet-btn/index.tsx
+++ components/wallet-btn/index.tsx
@@ -257,28 +257,28 @@ export default function WalletBtn({ name, address, onClickHandler }: Props) {
   );
 
   const walletconnect = React.useCallback(async () => {
-    // const providerOptions = {
-    //   walletconnect: {
-    //     package: WalletConnectProvider,
-    //     options: {
-    //       infuraId: INFURA_ID,
-    //     },
-    //   },
-    // };
-    // const web3Modal = new Web3Modal({
-    //   network: 'mainnet',
-    //   cacheProvider: true,
-    //   providerOptions,
-    // });
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: INFURA_ID,
+        },
+      },
+    };
+    const web3Modal = new Web3Modal({
+      network: 'mainnet',
+      cacheProvider: true,
+      providerOptions,
+    });
 
-    // const provider = await web3Modal.connect();
-    // if (provider.infuraId) {
-    //   await web3Modal.toggleModal();
-    //   const web_3 = new WalletConnectProvider(provider);
-    //   w3.current = web_3;
-    //   await subscribeProvider(provider, web_3, web3Modal);
-    //   return web_3;
-    // }
+    const provider = await web3Modal.connect();
+    if (provider.infuraId) {
+      await web3Modal.toggleModal();
+      const web_3 = new WalletConnectProvider(provider);
+      w3.current = web_3;
+      await subscribeProvider(provider, web_3, web3Modal);
+      return web_3;
+    }
     connectToChain();
   }, [subscribeProvider, connectToChain, w3]);
   const clickItem = React.useCallback(
