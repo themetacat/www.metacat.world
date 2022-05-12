5e1b2b77f6256cf920700902e6c855897945765f
张宇博 <9840072+ZhangYuBoYa@user.noreply.gitee.com>
2022-05-12 09:53:24 +0800

    fix: ceshi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1161,6 +1161,17 @@ export default function Index(props) {
   }, [requestPersonal]);
 
   React.useEffect(() => {
+    if (
+      navigator.userAgent.match(
+        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i,
+      )
+    ) {
+      // window.location.href="移动端url";
+      console.log('sj');
+    } else {
+      // window.location.href="pc端url";
+      console.log('pc');
+    }
     initTopic();
     init();
   }, [null]);
