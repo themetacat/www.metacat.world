0f88f588ab123f2c7cb7f55cbca6ddbae12c2603
wuqian <qianwu184@gmail.com>
2022-07-20 20:02:11 +0800

    fix: fix number label bug

--- components/v2/floor-price-card/index.tsx
+++ components/v2/floor-price-card/index.tsx
@@ -21,6 +21,30 @@ interface Props {
 export default function FloorPriceCard({ title, items, className }: Props) {
   const [data, setData] = React.useState([]);
 
+  // 乘
+  const floatMultiply = (arg1, arg2) => {
+    if (arg1 == null || arg2 == null || arg1 === 0) {
+      return 0;
+    }
+    let r1;
+    let r2; // 小数位数
+    try {
+      const s1 = arg1.toString();
+      r1 = s1.split('.')[1].length;
+    } catch (e) {
+      r1 = 0;
+    }
+    try {
+      const s2 = arg1.toString();
+      r2 = s2.split('.')[1].length;
+    } catch (e) {
+      r2 = 0;
+    }
+    const n1 = Number(arg1.toString().replace('.', ''));
+    const n2 = Number(arg2.toString().replace('.', ''));
+    return (n1 * n2) / 10 ** (r1 + r2);
+  };
+
   React.useEffect(() => {
     const result = items.map((x) => {
       const n = ICON_DATA.find((y) => y.world === x.world);
@@ -43,7 +67,6 @@ export default function FloorPriceCard({ title, items, className }: Props) {
         className={cn(' list-none w-full text-white text-sm py-8 px-5 overflow-hidden', className)}
       >
         {data.map((item, idx) => {
-          console.log(item);
           return item ? (
             <li key={idx} className="flex justify-between items-center mb-6">
               <div className="flex items-center w-40">
@@ -63,7 +86,7 @@ export default function FloorPriceCard({ title, items, className }: Props) {
                   item.precent >= 0 ? ' text-green-500' : ' text-red-500',
                   ' w-10 text-right',
                 )}
-              >{`${item.precent > 0 ? '+' : ''}${item.precent * 100}%`}</span>
+              >{`${item.precent > 0 ? '+' : ''}${floatMultiply(item.precent, 100)}%`}</span>
             </li>
           ) : null;
         })}
