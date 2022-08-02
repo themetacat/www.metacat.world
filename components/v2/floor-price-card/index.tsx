92fbc787343517768e607b192b9f801e73eddfe7
LiuJingZ <346197701@qq.com>
2022-08-02 14:45:06 +0800

    fix:修复已知bug

--- components/v2/floor-price-card/index.tsx
+++ components/v2/floor-price-card/index.tsx
@@ -60,9 +60,7 @@ export default function FloorPriceCard({ title, items, className }: Props) {
   }, [items]);
 
   return (
-    
     <AnalyticsCard title={title} backCls="cover1">
-      
       <ul
         className={cn(' list-none w-full text-white text-sm py-8 px-5 overflow-hidden', className)}
       >
