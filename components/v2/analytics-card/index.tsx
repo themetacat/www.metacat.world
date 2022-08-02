1e2d60d6cc5eb06aac29f026c149aacee7877a51
LiuJingZ <346197701@qq.com>
2022-08-02 15:58:39 +0800

    fix:修复已知bug

--- components/v2/analytics-card/index.tsx
+++ components/v2/analytics-card/index.tsx
@@ -8,7 +8,7 @@ import style from './index.module.css';
 interface Props {
   title?: string;
   link?: string;
-  children?: ReactElement|any;
+  children?:any;
   baseSize?: boolean;
   className?: string;
   backCls?: string;
