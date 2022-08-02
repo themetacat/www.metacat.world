f9381db06590d612c161883a88a3cc89aa09cd16
LiuJingZ <346197701@qq.com>
2022-08-02 14:51:42 +0800

    fix:修复已知bug

--- components/v2/floor-price-card/index.tsx
+++ components/v2/floor-price-card/index.tsx
@@ -42,7 +42,7 @@ export default function FloorPriceCard({ title, items, className }: Props) {
     const n2 = Number(arg2.toString().replace('.', ''));
     return (n1 * n2) / 10 ** (r1 + r2);
   };
-   
+      
   React.useEffect(() => {
     const result = items.map((x) => {
       const n = ICON_DATA.find((y) => y.world === x.world);
