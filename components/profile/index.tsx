86cc20cb7ec6dfad6025f30689913bf67e882c64
LiuJingZ <346197701@qq.com>
2022-08-12 19:38:25 +0800

    fix: 修改个人中心样式

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -49,6 +49,7 @@ export default function Profile({
         <div style={{ textAlign: 'center' }}>
           <img className={style.avater} src={avater || '/images/logo.png'}></img>
         </div>
+        
         <div className={cn('ml-8', style.info)}>
           <div className="flex">
             {address && address !== '' && name ? (
