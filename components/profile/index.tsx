04668ecc2d077bc95bbb7e0d3b05a4c713bdff20
LiuJingZ <346197701@qq.com>
2022-08-12 19:15:56 +0800

    fix: 修改样式

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -48,14 +48,7 @@ export default function Profile({
       <div >
         <div style={{ textAlign: 'center' }}>
           <img className={style.avater} src={avater || '/images/logo.png'}></img>
-          {/* <div style={{ color: '#fff', fontSize: '18px', display: 'flex', paddingLeft: '35px' }}>
-            <span>BCA Gallery</span>
-            <img
-              src={`/images/icon/bianji.png`}
-              style={{ width: '25px', height: '25px', marginLeft: '12px' }}
-            ></img>
-          </div> */}
-          {/* <div style={{ color: '#6a6f6e' }}>Area:Brazi</div> */}
+         
 
         </div>
         
