247b671666edde49001e13f01c4f98c56a42e211
LiuJingZ <346197701@qq.com>
2022-08-12 18:57:20 +0800

    fix: 修改样式

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -57,6 +57,7 @@ export default function Profile({
           </div> */}
           {/* <div style={{ color: '#6a6f6e' }}>Area:Brazi</div> */}
         </div>
+        
         <div className={cn('ml-8', style.info)}>
           <div className="flex">
             {address && address !== '' && name ? (
