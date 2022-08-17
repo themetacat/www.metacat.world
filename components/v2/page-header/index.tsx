909f88ca124d1293d02e528579f6ed6dce88a992
LiuJingZ <346197701@qq.com>
2022-08-17 18:23:02 +0800

    fix: 修改样式parcels

--- components/v2/page-header/index.tsx
+++ components/v2/page-header/index.tsx
@@ -281,7 +281,6 @@ export default function PageHeader({ active, className }: Props) {
               // onClick={() => {
               //   setBuildState(!buildState);
               // }}
-              
               onMouseEnter={() => {
                 setBuildState(true);
               }}
