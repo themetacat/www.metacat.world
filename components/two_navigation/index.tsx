2c4275a3507198bbda06e1c6cb55705b8c8fb0f8
LiuJingZ <346197701@qq.com>
2022-08-18 16:22:54 +0800

    fix: 创建新分支

--- components/two_navigation/index.tsx
+++ components/two_navigation/index.tsx
@@ -12,11 +12,25 @@ type Props = {
 export default function TwoNavigation({ options, className, location }: Props) {
   return (
     <div className={cn(location)}>
-      {options.map((item, index) => {
+        {options.map((item, index) => {
         return (
-          <div key={index} className={cn(style.item, className)}>
-            <Link href={item.link}>{item.label}</Link>
-          </div>
+          <Link href={item.link}>
+            <div key={index} className={cn(style.item, className)} style={{ display: 'flex' }}>
+             {item.icon?(<img
+                src={item.icon}
+                style={{
+                  width: '25px',
+                  height: '25px',
+                  marginRight:"8px",
+                  lineHeight: '30px',
+                  borderRadius: '50%',
+                  marginTop: '12px',
+                  border:"0.3px solid rgba(255,255,255,0.3)",
+                }}
+              ></img>):""} 
+              <Link href={item.link}>{item.label}</Link>
+            </div>
+          </Link>
         );
       })}
     </div>
