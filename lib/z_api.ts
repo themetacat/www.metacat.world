d354053c76963d1a4b59329d5279675716ea710a
LiuJingZ <346197701@qq.com>
2022-09-22 17:58:04 +0800

    fix: 更新url

--- lib/z_api.ts
+++ lib/z_api.ts
@@ -6,8 +6,8 @@ class API {
   private key?: string;
 
   constructor(url: string, key?: string) {
-    this.url = url;
-    // this.url = 'http://8.130.23.16/api/v1';
+    // this.url = url;
+    this.url = 'http://8.130.23.16/api/v1';
     this.key = key;
   }
 
