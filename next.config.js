c618a6c8a40f70114c68903a3d30a6fb3694c266
LiuJingZ <346197701@qq.com>
2023-03-07 16:57:36 +0800

    fix:

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // modifyVars: { '@primary-color': '#04f' },
+  // optional
+  lessVarsFilePath: './styles/antd.less',
+  // optional
+  // lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(gif|svg|jpg|png)$/,
+      loader: 'file-loader',
+    });
+    return config;
+  },
+
+  i18n: {
+    locales: ['zh-CN', 'en-US'],
+    defaultLocale: 'zh-CN',
+  },
+  images: {
+    domains: ['static.ghost.org'],
+  },
+});
