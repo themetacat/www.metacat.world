5b59a23b012608b18a1bbba9dd22ca0710cdec29
LiuJingZ <346197701@qq.com>
2023-05-15 12:38:22 +0800

    fix: 修复bug

--- lib/api.ts
+++ lib/api.ts
@@ -924,7 +924,7 @@ class API {
   }
 }
 
-export default new API('https://api.metacat.world/api/v1');
-// export default new API('http://8.130.23.16/api/v1');
+// export default new API('https://api.metacat.world/api/v1');
+export default new API('http://8.130.23.16/api/v1');
 // http://8.130.23.16/
 // https://api.metacat.world
