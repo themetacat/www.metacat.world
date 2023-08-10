3644f655e383158e41c79ff453956cc907954d06
LiuJingZ <346197701@qq.com>
2023-08-10 17:39:33 +0800

    fix: Optimize page

--- components/home-top/index.tsx
+++ components/home-top/index.tsx
@@ -764,12 +764,11 @@ export default function HomePage({ onClickHandler }: Props,ref) {
       const abi = [{"inputs":[{"internalType":"string","name":"newURI","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ApprovalCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"ApprovalQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"BalanceQueryForZeroAddress","type":"error"},{"inputs":[],"name":"MintERC2309QuantityExceedsLimit","type":"error"},{"inputs":[],"name":"MintToZeroAddress","type":"error"},{"inputs":[],"name":"MintZeroQuantity","type":"error"},{"inputs":[],"name":"OwnerQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"OwnershipNotInitializedForExtraData","type":"error"},{"inputs":[],"name":"TransferCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"TransferFromIncorrectOwner","type":"error"},{"inputs":[],"name":"TransferToNonERC721ReceiverImplementer","type":"error"},{"inputs":[],"name":"TransferToZeroAddress","type":"error"},{"inputs":[],"name":"URIQueryForNonexistentToken","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"fromTokenId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"toTokenId","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"ConsecutiveTransfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getInfo","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"send","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"newURI","type":"string"}],"name":"setURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"addr","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
       const mintBag =async ()=>{
         try {
-          console.log(111);
           
           if (typeof window.ethereum !== "undefined") {
             // 请求用户授权连接 MetaMask
             const networkVersion = window.ethereum.networkVersion;
-            console.log(networkVersion);
+            // console.log(networkVersion);
             
             if (networkVersion !== "80001") {
               console.log("有没有问题");
@@ -780,7 +779,7 @@ export default function HomePage({ onClickHandler }: Props,ref) {
               setLoading(true)
 
               await window.ethereum.request({ method: "eth_requestAccounts" });
-          console.log(window.ethereum.request({ method: "eth_requestAccounts" }));
+          // console.log(window.ethereum.request({ method: "eth_requestAccounts" }));
           
               // 创建 Web3 实例
               const web3Con = new Web3(window.ethereum);
@@ -793,7 +792,7 @@ export default function HomePage({ onClickHandler }: Props,ref) {
               // const balance = await web3Con.eth.getBalance(accountAddress);
               // 将 balance 单位由 wei 转换为以太币，并将其转换为数字类型
               // const ethBalance = Number(web3Con.utils.fromWei(balance, "ether"));
-              console.log(accountAddress, "accountAddres1111s");
+              // console.log(accountAddress, "accountAddres1111s");
             
     
               // 对比 ETH 余额和数量与单价的乘积
@@ -814,7 +813,7 @@ export default function HomePage({ onClickHandler }: Props,ref) {
                       
                 // const contractAddress = "0xed2a07b9b40acf575f0cf61475034a0ccf5bd29c";
                 const contract = new web3a.eth.Contract(abi as [], contractAddress);
-                console.log(contract.methods.ownerOf(1).call({}),3333);
+                // console.log(contract.methods.ownerOf(1).call({}),3333);
                 // const mintNums = num; // 在这里定义要mint的数量
                 setTimeout(() => {
                   setLoading(false);
@@ -831,25 +830,27 @@ export default function HomePage({ onClickHandler }: Props,ref) {
                   })
                   .on("receipt", (receipt) => {
                     // 获取交易收据
-                    console.log("收据: ", receipt);
+                    // console.log("收据: ", receipt);
                     router.replace('/bags')
                     // 在这里执行交易成功后的逻辑操作
                   })
                   .catch((error) => {
-                    console.log('错误错误错误！！！');
+                 
                     
                     if (error.code === 4001) {
+                      // console.log('错误错误错误！！！');
+                      
                       setLoading(false);
                     }
                   })
                   .then((success) => {
                     // if (success.code === 0x1||1) {
                     setLoading(false);
-                    console.log("成功",editNum,num);
+                    // console.log("成功",editNum,num);
                  
                     // }
                   });
-    
+                  setMintContent(false)
                 setLoading(false);
                 // contract.events
                 //   .Transfer(
@@ -878,8 +879,10 @@ export default function HomePage({ onClickHandler }: Props,ref) {
                 //   });
                 if (tx) {
                   toast.success("Successful");
+                  
                 }
                 setLoading(false);
+
                 console.log("tx:", tx);
                 // mint 成功, 进行后续操作
               } else {
@@ -1007,7 +1010,7 @@ web3.eth.getBalance(walletAddress)
       }, []);
 
       const iconSvg =()=>{
-        window.open('https://testnets.opensea.io/collection/wearablepack')
+        window.open('https://testnets.opensea.io/collection/wearablepack-1')
       }
 
 
