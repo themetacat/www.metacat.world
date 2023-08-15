7e96bf005ba870c41bdaeefd276a3c6af5dcacf9
LiuJingZ <346197701@qq.com>
2023-08-15 11:30:35 +0800

    fix: 检测添加网络

--- components/home-top/index.tsx
+++ components/home-top/index.tsx
@@ -927,7 +927,7 @@ export default function HomePage({ onClickHandler }: Props,ref) {
                   params: [
                     {
                       chainId: '0x13881',
-                      chainName: 'Mumbai Testnet',
+                      chainName: 'Mumbai',
                       nativeCurrency: {
                         name: 'MATIC',
                         symbol: 'MATIC',
